8a6797628fe9b1dd6eb99a78208f978dc649d816
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2017-12-11 19:05:00 +0100

    Added days 1 & 2

---
+++ AdventOfCode2017/Day2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace advent
+{
+    class Day2
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            while (true)
+            {
+                string row = Console.ReadLine();
+                if (row == "")
+                {
+                    break;
+                }
+
+                int lowest = int.MaxValue, highest = int.MinValue;
+
+                string[] s = row.Split();
+                foreach (string number in s)
+                {
+                    int actualDigit = Convert.ToInt32(number);
+                    if (actualDigit > highest)
+                    {
+                        highest = actualDigit;
+                    }
+
+                    if (actualDigit < lowest)
+                    {
+                        lowest = actualDigit;
+                    }
+                }
+                sum += (highest - lowest);
+            }
+
+            System.Console.WriteLine($"{sum}");
+        }
+    }
+}
